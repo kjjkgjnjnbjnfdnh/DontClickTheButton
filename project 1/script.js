b3b71aae7a156491220eb4a9c9c4bab1aee55e13
kjjkgjnjnbjnfdnh <97375393+kjjkgjnjnbjnfdnh@users.noreply.github.com>
2022-01-09 13:10:45 +0800

    Add files via upload

---
+++ project 1/script.js
@@ -0,0 +1,5 @@
+function changeBackground() {
+	document.body.style.backgroundImage = "url('rick.gif')";
+	document.querySelector(".everything").style.display = "none"
+	document.getElementById("song").play()
+}
